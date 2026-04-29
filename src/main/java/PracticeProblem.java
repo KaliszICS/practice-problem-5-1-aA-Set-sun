cbd9a1e2e03d4819773807d0915174dab5980a71
A.Set <343303426@tdsb.ca>
2026-04-29 11:42:04 -0400

    what was that

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,44 @@
+/*Title: Arrays 5.1
+Name: Angie Seto
+Date created: April 28, 2026
+Date updated: April 29, 2026*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+		int[] testArray = new int[3];
+		testArray[0] = 2;
+		testArray[1] = 9;
+		testArray[2] = 10; 
+		System.out.println(testArray[2]);
+
+	}
+//q1
+ 		public static int[] createIntArray() {
+			int[] arr = new int[5];
+
+			for (int i = 0; i < 5; i++) {
+				arr[i] = i + 1; //starts from 0, +1 every time, so it's 1-5
+			}
+			return arr;
+		}
+
+		//q2
+		public static String[] createArray (String one, String two, String three, String four) {
+
+			String[] arr = {one, two, three, four};
+			return arr;
+		}
+
+	//q3 (4??)
+	public static String getElement(int num, String[] arr) {
+		return arr[num];
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	//q 4 -> 5
+	public static double replaceElement(int num, double num1, double[] arr) {
+		arr[num] = num1;
+		return arr[num];
 	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
